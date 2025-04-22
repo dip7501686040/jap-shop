39de0dd6437ff7ba54f6a3c20cb33cec47cc982e
Dipankar saha <dip7501686040@gmail.com>
2025-04-22 18:58:34 +0530

    change

--- apps/admin-frontend/src/app/auth/login/page.tsx
+++ apps/admin-frontend/src/app/auth/login/page.tsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{" "}
               <Link href="/auth/signup" className="font-medium text-blue-600 hover:text-blue-500">
-                create a new account 11
+                create a new account 1
               </Link>
             </p>
           </div>
