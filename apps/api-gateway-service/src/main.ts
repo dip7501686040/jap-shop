c478d39cda636c248f4760fbbb0efa9c5b72322f
Dipankar saha <dip7501686040@gmail.com>
2025-04-22 22:25:48 +0530

    fix

--- apps/api-gateway-service/src/main.ts
+++ apps/api-gateway-service/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 4000;
   console.log(
-    `API Gateway Service is running on port ${port} with env 18 ${configService.get<string>('ENV')} test change`,
+    `API Gateway Service is running on port ${port} with env 19 ${configService.get<string>('ENV')} test change`,
   );
   await app.listen(port);
 }
