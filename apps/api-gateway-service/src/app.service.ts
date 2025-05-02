1779edb3fc6bf4035a6841634998897472110509
Dipankar saha <dip7501686040@gmail.com>
2025-05-02 19:20:21 +0530

    rollout update 222

--- apps/api-gateway-service/src/app.service.ts
+++ apps/api-gateway-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World rollout update 1';
+    return 'Hello World rollout update 222';
   }
 }
