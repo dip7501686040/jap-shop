5276f48bf33afc8a9c2f537a91b1fde0f6d1a756
Dipankar saha <dip7501686040@gmail.com>
2025-06-24 12:46:08 +0530

    Fix: api ingress annotation rewirte removed

--- apps/api-gateway-service/src/app.service.ts
+++ apps/api-gateway-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World rollout update 222';
+    return 'Hello World rollout update 115';
   }
 }
