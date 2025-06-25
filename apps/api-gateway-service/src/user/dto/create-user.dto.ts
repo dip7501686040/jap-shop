ba48fc2fc13f546b8ce6c6cc24ca7291e7cfe6c5
Dipankar saha <dip7501686040@gmail.com>
2025-06-25 09:18:12 +0530

    Fix: fixed dto payload issues of login and register api's

--- apps/api-gateway-service/src/user/dto/create-user.dto.ts
+++ apps/api-gateway-service/src/user/dto/create-user.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User email address',
     example: 'user@example.com',
   })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ description: 'User password', example: 'strongPassword123' })
+  @IsString()
+  @Length(8, 20)
   password: string;
 
   @ApiProperty({ description: 'User full name', example: 'John Doe' })
+  @IsString()
   name: string;
 }
