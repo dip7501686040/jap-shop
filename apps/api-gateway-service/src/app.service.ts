7c615c272d736edd116405c713deb12f46357e04
Dipankar saha <dip7501686040@gmail.com>
2025-06-27 08:25:06 +0530

    Fiix: Fixed start script for docker run issue

--- apps/api-gateway-service/src/app.service.ts
+++ apps/api-gateway-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World rollout update 117';
+    return 'Hello World rollout update 118';
   }
 }
