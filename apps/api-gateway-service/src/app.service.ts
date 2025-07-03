f2de31abef1515ec3dd0e0fae183e909e170ec1c
Dipankar saha <dip7501686040@gmail.com>
2025-07-03 19:03:09 +0530

    Fix: prisma generate fixed in dockerfile of api-gateway

--- apps/api-gateway-service/src/app.service.ts
+++ apps/api-gateway-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World rollout update 120';
+    return 'Hello World rollout update 121';
   }
 }
